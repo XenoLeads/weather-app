0a29a61cdac17c240267e04b4167898f777e83a2
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-09-30 20:01:05 +0530

    feat: search suggestions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,20 @@
 import "../styles/style.css";
 import Weather from "./weather-api.js";
+
+const searchInput = document.getElementById("search");
+const searchSuggestions = document.getElementById("search-suggestions");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.trim();
+  if (value.length < 3) return;
+  searchSuggestions.innerHTML = "";
+  Weather.geocode(value).then(geocode => {
+    if (!geocode) return;
+    geocode.forEach(location => {
+      const option = document.createElement("option");
+      option.value = location.name;
+      option.textContent = location.name;
+      searchSuggestions.appendChild(option);
+    });
+  });
+});
