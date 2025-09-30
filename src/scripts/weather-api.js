b9226488ed40bec801ba1648a2b311c1bc8a8d51
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-09-30 20:39:35 +0530

    feat(weather): add function to fetch weather data

--- src/scripts/weather-api.js
+++ src/scripts/weather-api.js
@@ -34,6 +34,22 @@ async function getGeocode(locationName) {
   return formattedLocationData;
 }
 
+// API call URL:
+// `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_min,temperature_2m_max,temperature_2m_mean,apparent_temperature_mean,relative_humidity_2m_mean,surface_pressure_mean,wind_speed_10m_mean,sunrise,sunset,uv_index_max,wind_direction_10m_dominant,precipitation_probability_mean,weather_code&hourly=temperature_2m,apparent_temperature,weather_code,relative_humidity_2m,surface_pressure,wind_speed_10m,precipitation_probability,uv_index,wind_direction_10m&current=relative_humidity_2m,temperature_2m,apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m,weather_code,surface_pressure&timezone=${timezone}`
+// Get weather data using location name
+async function getWeatherData(locationName) {
+  const locationData = await getGeocode(locationName);
+  if (!locationData) throw new Error("Location not found!");
+  const { latitude, longitude } = locationData[0];
+  const timezone = locationData[0].replace("/", "%2F");
+  const weatherResponse = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_min,temperature_2m_max,temperature_2m_mean,apparent_temperature_mean,relative_humidity_2m_mean,surface_pressure_mean,wind_speed_10m_mean,sunrise,sunset,uv_index_max,wind_direction_10m_dominant,precipitation_probability_mean,weather_code&hourly=temperature_2m,apparent_temperature,weather_code,relative_humidity_2m,surface_pressure,wind_speed_10m,precipitation_probability,uv_index,wind_direction_10m&current=relative_humidity_2m,temperature_2m,apparent_temperature,precipitation,wind_speed_10m,wind_direction_10m,weather_code,surface_pressure&timezone=${timezone}`
+  );
+  const weatherResponseJson = await weatherResponse.json();
+  return weatherResponseJson;
+}
+
 export default {
   geocode: getGeocode,
+  get: getWeatherData,
 };
