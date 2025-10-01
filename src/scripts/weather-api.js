273cb1d72391a0f9b850d36b8713ccc32e11543d
Xeno <226741508+XenoLeads@users.noreply.github.com>
2025-10-01 15:27:03 +0530

    feat: implement api data formatter

--- src/scripts/weather-api.js
+++ src/scripts/weather-api.js
@@ -1,3 +1,34 @@
+const WEATHER_CODES = {
+  0: "Clear Sky",
+  1: "Mainly Clear",
+  2: "Partly Cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing Rime Fog",
+  51: "Light Drizzle",
+  53: "Moderate Drizzle",
+  55: "Dense Drizzle",
+  56: "Light Freezing Drizzle",
+  57: "Dense Freezing Drizzle",
+  61: "Slight Rain",
+  63: "Moderate Rain",
+  65: "Heavy Rain",
+  66: "Light Freezing Rain",
+  67: "Heavy Freezing Rain",
+  71: "Slight Snowfall",
+  73: "Moderate Snowfall",
+  75: "Heavy Snowfall",
+  77: "Snow Grains",
+  80: "Slight Rain Showers",
+  81: "Moderate Rain Showers",
+  82: "Violent Rain Showers",
+  85: "Slight Snow Showers",
+  86: "Heavy Snow Showers",
+  95: "Slight or Moderate Thunderstorm",
+  96: "Thunderstorm with Slight Hail",
+  99: "Thunderstorm with Heavy Hail",
+};
+
 // Get necessary data from location name
 async function getLocationData(locationName) {
   const response = await fetch(
@@ -47,6 +78,144 @@ async function getWeatherData(locationName) {
   );
   const weatherResponseJson = await weatherResponse.json();
   return weatherResponseJson;
+}
+
+function formatWeatherData(data) {
+  const formattedData = { current: {}, current_units: {}, daily: [], daily_units: {}, hourly_units: {} };
+  for (let i = 0; i < data.daily.weather_code.length; i++) formattedData.daily.push({ hourly: [] });
+  for (let i = 0; i < formattedData.daily.length; i++) {
+    for (let j = 0; j < 24; j++) {
+      formattedData.daily[i].hourly.push({});
+    }
+  }
+
+  const keyMap = {
+    temperature_2m: "temperature",
+    temperature_2m_mean: "temperature",
+    temperature_2m_min: "temperature_min",
+    temperature_2m_max: "temperature_max",
+    apparent_temperature_mean: "temperature_feelslike",
+    apparent_temperature: "temperature_feelslike",
+    relative_humidity_2m_mean: "humidity",
+    relative_humidity_2m: "humidity",
+    precipitation_probability_mean: "precipitation",
+    precipitation_probability: "precipitation",
+    precipitation: "precipitation",
+    surface_pressure_mean: "pressure",
+    surface_pressure: "pressure",
+    uv_index_max: "uv_index",
+    uv_index: "uv_index",
+    wind_speed_10m_mean: "wind_speed",
+    wind_speed_10m: "wind_speed",
+    wind_direction_10m_dominant: "wind_direction",
+    wind_direction_10m: "wind_direction",
+    sunrise: "sunrise",
+    sunset: "sunset",
+    time: "time",
+    weather_code: "condition",
+  };
+
+  // Format daily weather data
+  for (const key in data.daily) {
+    if (keyMap[key]) {
+      if (keyMap[key] === "condition") {
+        for (let i = 0; i < data.daily[key].length; i++) {
+          formattedData.daily[i][keyMap[key]] = WEATHER_CODES[data.daily[key][i]];
+        }
+      } else if (keyMap[key] === "time") {
+        for (let i = 0; i < data.daily[key].length; i++) {
+          formattedData.daily[i]["date"] = data.daily[key][i];
+        }
+      } else {
+        for (let i = 0; i < data.daily[key].length; i++) {
+          formattedData.daily[i][keyMap[key]] = data.daily[key][i];
+        }
+      }
+    }
+  }
+
+  // Format hourly weather data
+  for (const key in data.hourly) {
+    if (keyMap[key]) {
+      if (keyMap[key] === "condition") {
+        for (let i = 0; i < formattedData.daily.length; i++) {
+          const weather_codes = data.hourly[key].slice(i * 24, i * 24 + 24);
+          for (let j = 0; j < formattedData.daily[i].hourly.length; j++) {
+            formattedData.daily[i].hourly[j][keyMap[key]] = WEATHER_CODES[weather_codes[j]];
+          }
+        }
+      } else {
+        for (let i = 0; i < formattedData.daily.length; i++) {
+          const hours = data.hourly[key].slice(i * 24, i * 24 + 24);
+          for (let j = 0; j < formattedData.daily[i].hourly.length; j++) {
+            formattedData.daily[i].hourly[j][keyMap[key]] = hours[j];
+          }
+        }
+      }
+    }
+  }
+  // Copy missing properties from daily weather data
+  const hourly_keys = Object.keys(formattedData.daily[0].hourly[0]);
+  for (const key in formattedData.daily[0]) {
+    if (!hourly_keys.includes(key) && key !== "hourly") {
+      for (let i = 0; i < formattedData.daily.length; i++) {
+        for (let j = 0; j < formattedData.daily[i].hourly.length; j++) {
+          formattedData.daily[i].hourly[j][key] = formattedData.daily[i][key];
+        }
+      }
+    }
+  }
+
+  // Format current weather data
+  for (const key in data.current) {
+    if (keyMap[key] === "precipitation") {
+      formattedData.current[keyMap[key]] = formattedData.daily[0][keyMap[key]];
+    } else if (keyMap[key] === "condition") {
+      formattedData.current[keyMap[key]] = WEATHER_CODES[data.current[key]];
+    } else if (keyMap[key]) {
+      formattedData.current[keyMap[key]] = data.current[key];
+    }
+  }
+
+  // Format daily weather data units
+  for (const key in data.daily_units) {
+    if (keyMap[key]) {
+      if (keyMap[key] === "condition") {
+        formattedData.daily_units[keyMap[key]] = "";
+      } else if (keyMap[key] === "time") {
+        formattedData.daily_units["date"] = data.daily_units[key];
+      } else {
+        formattedData.daily_units[keyMap[key]] = data.daily_units[key];
+      }
+    }
+  }
+
+  // Format current weather data units
+  for (const key in data.current_units) {
+    if (keyMap[key] === "precipitation") {
+      formattedData.current_units[keyMap[key]] = formattedData.daily_units[keyMap[key]];
+    } else if (keyMap[key] === "condition") {
+      formattedData.current_units[keyMap[key]] = "";
+    } else if (keyMap[key]) {
+      formattedData.current_units[keyMap[key]] = data.current_units[key];
+    }
+  }
+
+  // Format hourly weather data units
+  for (const key in data.hourly_units) {
+    if (keyMap[key] === "condition") {
+      formattedData.hourly_units[keyMap[key]] = "";
+    } else if (keyMap[key]) {
+      formattedData.hourly_units[keyMap[key]] = data.hourly_units[key];
+    }
+  }
+  // Copy missing properties from daily weather data
+  const hourly_units_keys = Object.keys(formattedData.hourly_units);
+  for (const key in formattedData.daily_units) {
+    if (!hourly_units_keys.includes(key)) {
+      formattedData.hourly_units[key] = formattedData.daily_units[key];
+    }
+  }
 }
 
 export default {
